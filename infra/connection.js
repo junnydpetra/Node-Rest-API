7dacbe827b8e8f1a15d360fbc486dd5655e9bbaa
junnydpetra <junnycmenezes@gmail.com>
2024-12-26 14:06:45 -0300

    database connected

--- infra/connection.js
+++ infra/connection.js
@@ -1,1 +1,11 @@
-.
+import mysql from 'mysql'
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'JunnyJunny12-',
+    database: 'db_api_rest'
+})
+
+export default connection
--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import app from "./src/app.js"
+import connection from "./infra/connection.js"
 
 const PORT = 3000
-
-/* listening the port */
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`)
+connection.connect((error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('Connection successful!');        
+        /* listening the port */
+        app.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}`)
+        })
+    }
 })
