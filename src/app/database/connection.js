c3eeea491d411f038174ffbff21fee6fd9add0b3
junnydpetra <junnycmenezes@gmail.com>
2024-12-30 01:03:42 -0300

    Optimization and refactoring

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -10,4 +10,21 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
+/**
+ * Run SQL code containing or not values
+ * @param {string} sql query to be executed 
+ * @param {string=id | [team, id]} values ​​to be passed to sql
+ * @param {*} rejectMessage message to be displayed in case of rejection
+ * @returns promise object
+ */
+export const data_query = (sql, values='', rejectMessage) => {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, values, (error, result) => {
+            if (error) return reject (rejectMessage)
+            const row = JSON.parse(JSON.stringify(result)) 
+            return resolve(row)
+        })
+    })
+}
+
 export default connection
--- src/app/repositories/TeamRepository.js
+++ src/app/repositories/TeamRepository.js
@@ -1,73 +1,30 @@
-import connection from '../database/connection.js'
+import { data_query } from "../database/connection.js"
 
 class TeamRepository {
 
     create(team){
         const sql = "INSERT INTO tab_teams SET ?;"
-        return new Promise((resolve, reject) => {
-            connection.query(sql, team, (error, result) => {
-                if (error) return reject ('Error registering team!')
-
-                /* Parse result */
-                const row = JSON.parse(JSON.stringify(result)) 
-                return resolve(result)
-            })
-        })
+        return data_query(sql, team, 'Unable to register team!')
     }
     
     findAll(){        
         const sql = "SELECT * FROM tab_teams;"
-        return new Promise((resolve, reject) => {
-            connection.query(sql, (error, result) => {
-                if (error) return reject ('Team not found!')
-
-                /* Parse result */
-                const row = JSON.parse(JSON.stringify(result)) 
-                return resolve(result)
-            })
-        })
+        return data_query(sql, 'Team registrations not found!')
     }
 
     findById(id){
         const sql = "SELECT * FROM tab_teams WHERE tea_id=?;"
-        return new Promise((resolve, reject) => {
-            connection.query(sql, id, (error, result) => {
-                if (error) return reject ('Team not found!')
-
-                /* Parse result */
-                const row = JSON.parse(JSON.stringify(result)) 
-                return resolve(row)
-            })
-
-        })
+        return data_query(sql, id, 'Team not found!')
     }
 
     update(team, id){
         const sql = "UPDATE tab_teams SET ? WHERE tea_id=?;"
-        return new Promise((resolve, reject) => {
-            connection.query(sql, [team, id], (error, result) => {
-                if (error) return reject ('Unable to update team registration!')
-
-                /* Parse result */
-                const row = JSON.parse(JSON.stringify(result)) 
-                return resolve(result)
-            })
-
-        })
+        return data_query(sql, [team, id], 'Unable to update team record!')
     }
 
     delete(id){
         const sql = "DELETE FROM tab_teams WHERE tea_id=?;"
-        return new Promise((resolve, reject) => {
-            connection.query(sql, id, (error, result) => {
-                if (error) return reject ('Unable to delete team registration!')
-
-                /* Parse result */
-                const row = JSON.parse(JSON.stringify(result)) 
-                return resolve(row)
-            })
-
-        })
+        return data_query(sql, id, 'Unable to delete team record!')
     }
 
 }
